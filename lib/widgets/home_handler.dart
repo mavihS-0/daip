14b6c10081fbc12d121bce49a7dfd3d1bfd34aa8
mavihS-0 <shivam9753singh@gmail.com>
2024-01-23 02:16:03 +0530

    adding icons (ieee and epics)

--- lib/widgets/home_handler.dart
+++ lib/widgets/home_handler.dart
@@ -13,6 +13,22 @@ class HomeWidget extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
           child: Text("DAIP"),
         ),
+        // actions: [
+        //   Padding(
+        //     padding: const EdgeInsets.all(8.0),
+        //     child: Image.asset('images/epics.webp',fit: BoxFit.fitHeight,height: 20,),
+        //   ),
+        //   Padding(
+        //     padding: const EdgeInsets.all(8.0),
+        //     child: Image.asset('images/ieee.png',fit: BoxFit.fitHeight,height: 30,),
+        //   ),
+        // ]
+        actions: [
+          IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.info_outline,size: 25,)
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
